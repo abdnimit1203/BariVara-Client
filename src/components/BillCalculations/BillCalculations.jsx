1b9516f5fa96caf87d0ee209bf674c707d984f6a
abdnimit1203 <abdullah.ibneali152pro@gmail.com>
2025-02-01 02:49:36 +0600

    updated date  on modal added

--- src/components/BillCalculations/BillCalculations.jsx
+++ src/components/BillCalculations/BillCalculations.jsx
@@ -22,7 +22,7 @@ const BillCalculations = ({
   const closeModal = () => {
     setIsModalOpen(false);
   };
-  console.log("Monthly Data:",myData);
+  console.log("Monthly Data:", myData);
 
   // ------------------- PRINT --------------------
   const printRef = useRef();
@@ -40,7 +40,11 @@ const BillCalculations = ({
     <div className="flex flex-col justify-center ">
       <div className="">
         <p className="flex justify-center text-center gap-2">
-          <FaSackDollar className={`${myData?.paid === "false" ? "text-error":"text-secondary"} inline text-xl `}/>
+          <FaSackDollar
+            className={`${
+              myData?.paid === "false" ? "text-error" : "text-secondary"
+            } inline text-xl `}
+          />
           {myData?.roomNo == undefined ? (
             <span>1800 টাকা</span>
           ) : (
@@ -249,9 +253,12 @@ const BillCalculations = ({
           {myData?.paid == "false" ? (
             <PaymentForm billID={myData._id} refetch4={refetch4} />
           ) : (
-            <p className="btn-success btn btn-sm mt-2 text-white">
-              Already paid
-            </p>
+            <>
+              <p className="btn-success btn btn-sm mt-2 text-white">
+                Already paid
+              </p>
+              <span className="block  text-xs opacity-60">Last Updated: {myData?.updatedAt}</span>
+            </>
           )}
         </UniversalModal>
       </div>
